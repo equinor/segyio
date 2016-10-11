bf57ddbbcb219a9b4ad6a89973947d6da1137db3
Thorvald Johannessen <thorvjo@statoil.com>
2016-10-11 11:27:58 +0200

    refactoring. modularizing viewer
    Classes are renamed and splitted up into seperate files, for easier use outside the example-application segyviewer. I.e to plot slices without QT.

---
+++ python/segyview/__init__.py
@@ -0,0 +1,10 @@
+from .segyplot import SegyPlot
+
+try:
+    from .linenavigationbar import LineNavigationBar
+    from .slicewidget import SliceWidget
+except ImportError as e:
+    import sys
+    import traceback
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=sys.stderr)
