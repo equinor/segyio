fc7588a37a1e930b3ecab4ce3f01fb7d1e56e070
Thorvald Johannessen <thorvjo@statoil.com>
2016-10-12 10:00:38 +0200

    Extracted read_all_traces to a seperate  util module
    To ease test setup, and modularize

--- applications/segyviewer.py
+++ applications/segyviewer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 import sys
-
-import numpy as np
 import segyio
 from PyQt4 import QtGui, QtCore
 from segyview import *
+from segyview import util
 
 
 class LineSelectionMonitor(QtCore.QObject):
@@ -79,7 +78,7 @@ class SegyViewer(QtGui.QMainWindow):
         self.addToolBar(LineNavigationBar(s.xlines, s.ilines, range(s.samples), line_monitor))
         self.statusBar()
 
-        depth_slices, min_max = read_traces_to_memory(s)
+        depth_slices, min_max = util.read_traces_to_memory(s)
 
         # initialize
         x_slice_widget = SliceWidget(s.xline, s.xlines,
@@ -139,40 +138,6 @@ class SegyViewer(QtGui.QMainWindow):
         main_widget.setFocus()
         self.setCentralWidget(main_widget)
         main_widget.hide()
-
-
-def read_traces_to_memory(segy):
-    """ Read all traces into memory and identify global min and max values.
-
-    Utility method to handle the challenge of navigating up and down in depth slices,
-    as each depth slice consist of samples from all traces in the segy file.
-
-    The cube returned is transposed in aspect of the depth plane. Where each slice of the returned array
-    consists of all samples for the given depth, oriented by [iline, xline]
-    """
-
-    all_traces = np.empty(shape=((len(segy.ilines) * len(segy.xlines)), segy.samples), dtype=np.float32)
-
-    min_value = sys.float_info.max
-    max_value = sys.float_info.min
-
-    for i, t in enumerate(segy.trace):
-        all_traces[i] = t
-
-        local_min = np.nanmin(t)
-        local_max = np.nanmax(t)
-
-        if np.isfinite(local_min):
-            min_value = min(local_min, min_value)
-
-        if np.isfinite(local_max):
-            max_value = max(local_max, max_value)
-
-    reshaped_traces = all_traces.reshape(len(segy.ilines), len(segy.xlines), segy.samples)
-
-    transposed_traces = reshaped_traces.transpose(2, 0, 1)
-
-    return transposed_traces, (min_value, max_value)
 
 
 def main():
