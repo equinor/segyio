af7bb2d430a7b99a47456dab17d1b2540bff7287
Jørgen Kvalsvik <jokva@statoil.com>
2017-02-01 16:54:25 +0100

    and right binary

--- cmake/FindJuliaInterp.cmake
+++ cmake/FindJuliaInterp.cmake
@@ -4,12 +4,11 @@ find_program(JULIA_EXECUTABLE NAMES ${_Julia_NAMES})
 
 if(NOT JULIA_EXECUTABLE)
     foreach(_CURRENT_VERSION "0.4 0.5")
-        find_program(PYTHON_EXECUTABLE
-            NAMES ${_Python_NAMES}
+        find_program(JULIA_EXECUTABLE
+            NAMES julia
             PATHS [HKEY_LOCAL_MACHINE\\SOFTWARE\\Julia\\${_CURRENT_VERSION}\\InstallPath]
             )
     endforeach()
-
 endif()
 
 if(JULIA_EXECUTABLE)
