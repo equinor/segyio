cdffa67c2c687c4cbcc8ef94c28b5968bd654630
Jørgen Kvalsvik <jokva@statoil.com>
2017-02-27 09:40:34 +0100

    Accelerate mex/get_header
    The C function segy_field_forall is a perfect fit for the matlab
    get_header function. Same function, but faster.

--- mex/segy_get_header_mex.c
+++ mex/segy_get_header_mex.c
@@ -11,51 +11,36 @@
 void mexFunction(int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[]) {
 
-    char* msg1;
-    char* msg2;
+    char* msg1 = "";
+    char* msg2 = "";
     int err;
 
     const char* filename = mxArrayToString( prhs[ 0 ] );
     segy_file* fp = segyfopen( prhs[ 0 ], "rb" );
+
+    if( !fp ) goto cleanup;
 
     int field = mxGetScalar( prhs[ 1 ] );
 
     struct segy_file_format fmt = filefmt( fp );
 
     plhs[0] = mxCreateNumericMatrix( 1, fmt.traces, mxINT32_CLASS, mxREAL );
-    int32_t* out = mxGetData( plhs[ 0 ] );
+    int* out = mxGetData( plhs[ 0 ] );
 
-    char header[ SEGY_TRACE_HEADER_SIZE ];
 
-    for( int i = 0; i < fmt.traces; ++i ) {
-        int32_t f;
-        err = segy_traceheader( fp, i, header, fmt.trace0, fmt.trace_bsize );
+    err = segy_field_forall( fp, field,
+                             0, fmt.traces, 1, /* start, stop, step */
+                             out, fmt.trace0, fmt.trace_bsize );
 
-        if( err != 0 ) {
-            msg1 = "segy:get_header:segy_traceheader";
-            msg2 = strerror( errno );
-            goto cleanup;
-        }
-
-        err = segy_get_field( header, field, &f );
-
-        if( err != 0 ) {
-            msg1 = "segy:get_header:segy_get_field";
-            msg2 = "Error reading header field.";
-            goto cleanup;
-        }
-
-        out[ i ] = f;
-    }
-
+    int no = errno;
     segy_close( fp );
+
+    if( err != SEGY_OK )
+        mexErrMsgIdAndTxt( "segy:get_header:forall", strerror( errno ) );
 
     plhs[ 1 ] = mxCreateDoubleScalar( fmt.traces );
     return;
 
 cleanup:
-    segy_close( fp );
-
-cleanup_fopen:
-    mexErrMsgIdAndTxt( msg1, msg2 );
+    mexErrMsgIdAndTxt( "segy:get_header:fopen", strerror( errno ) );
 }
