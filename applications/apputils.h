d666b8141cf75bbd3801a25d31580b56013b71c4
Jørgen Kvalsvik <jokva@statoil.com>
2017-08-08 14:25:23 +0200

    Move common functionality for apps into own lib
    Some functionality is necessary for most applications (error messages,
    number parsing etc.). Organise this in the apputils library to reduce
    duplication across binaries.

---
+++ applications/apputils.h
@@ -0,0 +1,10 @@
+#ifndef SEGYIO_APPUTILS_H
+#define SEGYIO_APPUTILS_H
+
+int errmsg( int errcode, const char* msg );
+int errmsg2( int errcode, const char* prelude, const char* msg );
+int parseint( const char* str, int* x );
+int bfield( const char* header, int field );
+int trfield( const char* header, int field );
+
+#endif //SEGYIO_APPUTILS_H
