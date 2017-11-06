c350acac667dd520952053ebedd1122eebd260d0
Jørgen Kvalsvik <jokva@statoil.com>
2017-11-06 10:53:14 +0100

    Set warnings with gen-expr; declare in root cmake
    Use generator expressions to switch warnings on/off on the targets
    instead of modifying global definitions (leaving this to the caller),
    and set the default set of warnings in debug mode in the root
    CMakeLists.txt rather than in the hidden cmake/ dir.

--- cmake/default_warnings.cmake
+++
@@ -1,9 +0,0 @@
-if (NOT MSVC)
-    # assuming gcc-style options
-    set(C_WARNINGS "-Wall -Wextra -pedantic -Wformat-nonliteral -Wcast-align -Wpointer-arith -Wmissing-declarations -Wcast-qual -Wshadow -Wwrite-strings -Wchar-subscripts -Wredundant-decls")
-    set(CMAKE_C_FLAGS_DEBUG "${C_WARNINGS} ${CMAKE_C_FLAGS_DEBUG}")
-    set(CMAKE_C_FLAGS_RELWITHDEBINFO "${C_WARNINGS} ${CMAKE_C_FLAGS_RELWITHDEBINFO}")
-else() 
-    add_definitions("/W3 /wd4996")
-endif ()
-
