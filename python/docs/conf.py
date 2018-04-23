81d353a0c5c96fab30b4413b3096a13414d0cc70
Jørgen Kvalsvik <jokva@statoil.com>
2018-04-23 11:53:03 +0200

    Don't mock the native extension
    Since the extension is lazily loaded, there's no need to mock it
    anymore.

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -24,7 +24,6 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../"))
-autodoc_mock_imports = ['segyio._segyio']
 
 # -- General configuration ------------------------------------------------
 
