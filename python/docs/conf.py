01dbe56b192b2c01ce8b0bda3364cf9ea2a196ee
Jørgen Kvalsvik <jokva@statoil.com>
2018-05-27 20:34:11 +0200

    Don't add module names in python docs

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -44,6 +44,7 @@ extensions = [
 ]
 
 autosummary_generate = True
+add_module_names = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
