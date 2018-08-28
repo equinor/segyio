a9e6468c580a4ad5c90e27690e609fb7c9ae7efe
Jørgen Kvalsvik <jokva@statoil.com>
2018-08-28 10:45:27 +0200

    Use curl instead of wget in manylinux image
    Wget links to the system openssl that does not work with TLS 1.2, which
    is now required to get the cmake binary. Replacing it with curl works.
    
    For details, see [1]
    
    [1]: https://github.com/pypa/manylinux/issues/202

--- config.sh
+++ config.sh
@@ -24,7 +24,7 @@ function pre_build {
     # not provide libc.i686 by default
     yum install -y glibc.i686
     export cmake=cmake-2.8.12.2-Linux-i386
-    wget --no-check-certificate https://cmake.org/files/v2.8/cmake-2.8.12.2-Linux-i386.tar.gz
+    curl -o $cmake.tar.gz https://cmake.org/files/v2.8/cmake-2.8.12.2-Linux-i386.tar.gz
     tar xzvf $cmake.tar.gz
     ./$cmake/bin/cmake --version
     ./$cmake/bin/cmake .. -DBUILD_PYTHON=OFF -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
