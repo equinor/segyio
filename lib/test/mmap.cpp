6e84e4df70a9ad89df750850e1f8eac072c8ec3d
Jørgen Kvalsvik <jokva@equinor.com>
2018-10-21 17:37:52 +0200

    Parameterise tests as separate processes, not tags
    Replace the include src.cpp way of parameterising tests with multiple
    processes, a global config object, and command line options.
    
    Every include would essentially add a constant compile time increase,
    which is over time unsustainable (I measured compilation time to approx
    3.5s per file on my laptop, which is excessive for 1500 lines).
    
    Additionally, it made code ugly because every test tag has to be unique,
    which was solved by defining a tag and prepending it to every test. This
    does not compose too nicely, and would get increasingly worse.
    
    There *will* be more cases of tests essentially being identical, except
    some slight configuration change on the file handle. It's desirable to
    run also these configurations through the full test suite.
    
    Running the multiple configurations is now handled by cmake, rather than
    implicitly by catch and the compiler.

--- lib/test/mmap.cpp
+++
@@ -1,6 +0,0 @@
-#ifdef HAVE_MMAP
-
-#define MMAP_TAG "[mmap] "
-#include "segy.cpp"
-
-#endif //HAVE_MMAP
--- lib/test/testsuite.cpp
+++ lib/test/testsuite.cpp
@@ -1,2 +1,37 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch/catch.hpp>
+
+#include <segyio/segy.h>
+
+#include "test-config.hpp"
+
+void testcfg::apply( segy_file* fp ) {
+    if( this->memmap ) {
+    #ifdef HAVE_MMAP
+        REQUIRE( segy_mmap( fp ) == SEGY_OK );
+    #endif //HAVE_MMAP
+    }
+}
+
+testcfg& testcfg::config() {
+    static testcfg s;
+    return s;
+}
+
+int main( int argc, char** argv ) {
+      Catch::Session session;
+
+      auto& cfg = testcfg::config();
+
+      using namespace Catch::clara;
+      auto cli = session.cli()
+          | Opt( cfg.memmap ) ["--test-mmap"] ("run with memory mapped files")
+          ;
+      session.cli( cli );
+
+      const int errc = session.applyCommandLine( argc, argv );
+      if( errc )
+          return errc;
+
+      return session.run();
+}
