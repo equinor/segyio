f8d38ded05f1be27a4428d9037c3ddc3150e95e5
Erlend Hårstad (IT SI SIB) <erha@statoil.com>
2018-10-29 09:17:21 +0100

    Let interpret() infer geometry in segyio.open()

--- python/segyio/open.py
+++ python/segyio/open.py
@@ -11,28 +11,12 @@ def infer_geometry(f, metrics, iline, xline, strict):
         offset_count = cube_metrics['offset_count']
         metrics.update(cube_metrics)
 
-        line_metrics = segyio._segyio.line_metrics(f.sorting,
-                                                   f.tracecount,
-                                                   iline_count,
-                                                   xline_count,
-                                                   offset_count)
+        ilines  = numpy.zeros(iline_count,  dtype=numpy.intc)
+        xlines  = numpy.zeros(xline_count,  dtype=numpy.intc)
+        offsets = numpy.zeros(offset_count, dtype=numpy.intc)
 
-        f._iline_length = line_metrics['iline_length']
-        f._iline_stride = line_metrics['iline_stride']
-
-        f._xline_length = line_metrics['xline_length']
-        f._xline_stride = line_metrics['xline_stride']
-
-        f._ilines  = numpy.zeros(iline_count,  dtype = numpy.intc)
-        f._xlines  = numpy.zeros(xline_count,  dtype = numpy.intc)
-        f._offsets = numpy.zeros(offset_count, dtype = numpy.intc)
-        f.xfd.indices(metrics, f.ilines, f.xlines, f.offsets)
-
-        if numpy.unique(f.ilines).size != f.ilines.size:
-            raise ValueError( "Inlines inconsistent - expect all inlines to be unique")
-
-        if numpy.unique(f.xlines).size != f.xlines.size:
-            raise ValueError( "Crosslines inconsistent - expect all crosslines to be unique")
+        f.xfd.indices(metrics, ilines, xlines, offsets)
+        f.interpret(ilines, xlines, offsets, f._sorting)
 
     except:
         if not strict:
@@ -44,6 +28,7 @@ def infer_geometry(f, metrics, iline, xline, strict):
             raise
 
     return f
+
 
 def open(filename, mode="r", iline = 189,
                              xline = 193,
