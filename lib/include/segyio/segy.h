66fa173ab9d6ed6178e3409af0fb47aded35947d
Yngve Villanger <yvi@equinor.com>
2025-05-08 10:43:21 +0200

    Extend segy_field_value to 8 bytes

--- lib/include/segyio/segy.h
+++ lib/include/segyio/segy.h
@@ -38,9 +38,13 @@ typedef union {
     uint8_t u8;
     uint16_t u16;
     uint32_t u32;
+    uint64_t u64;
     int8_t i8;
     int16_t i16;
     int32_t i32;
+    int64_t i64;
+    float f32;
+    double f64;
 } segy_field_value;
 
 typedef struct {
