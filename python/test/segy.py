766ef56a5b2b4089fd84aee5f5db1baafd95efe3
Yngve Villanger <yvi@equinor.com>
2025-06-16 09:10:04 +0200

    Fix Unassigned1 in Python

--- python/test/segy.py
+++ python/test/segy.py
@@ -338,10 +338,10 @@ def test_header_dict_methods(openfn, kwargs):
         assert segyio.su.cdpx in f.header[0]
         iter(f.header[0])
 
-        assert 35 == len(f.bin.keys())
-        assert 35 == len(list(f.bin.values()))
-        assert 35 == len(list(f.bin.items()))
-        assert 35 == len(f.bin)
+        assert 36 == len(f.bin.keys())
+        assert 36 == len(list(f.bin.values()))
+        assert 36 == len(list(f.bin.items()))
+        assert 36 == len(f.bin)
         iter(f.bin)
 
 
