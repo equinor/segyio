c32e7eaf8e6e438d7454acaced7d09c9828e1ddb
Alexander Jaust <18347253+ajaust@users.noreply.github.com>
2025-10-10 14:53:29 +0200

    Ignore some compiler warnings in Python bindings
    Disable warnings for methods exposed to Python arguments and potentially
    missing field initializers in Python-specific data structures. Removing
    the cause of the warnings is, in parts, impossible, or tedious as it
    depends on the Python version the Python bindings are compiled for.
    
    Note: The warnings became visible because we updated the build process
    of the Python bindings in a previous commit. Earlier, additional
    compiler flags for warnings and making warnings into errors were not
    forwarded to the Python binding's build process but are forwarded now.
    
    **Methods exposed to Python (-Wcast-function-type)**
    
    The PyMethodDef always expects a pointer to a C implementation of type
    `PyCFunction` as second argument. `PyCFunction` expects two input
    arguments. If a method has only one argument, e.g., a method that only
    accepts `self`, or a method with more than two arguments, e.g., a Python
    method with keyword arguments, the compiler issues a warning. As far as
    I understand, this cannot be resolved for functions with keyword
    arguments (`METH_VARARGS | METH_KEYWORDS`, see [1]) that have three
    input arguments. We can either cast the method to
    `PyCFunctionWithKeywords` and get an error about too many input
    arguments or we cast to PyCFunction and get an error about non-matching
    function types.
    
    **Missing field initalizers (-Wmissing-field-initializers)**
    
    The number of fields in structs defined by the Python interface depend
    on the version number. To fix this we would need, potentialy, confusing
    `#if ... #else` constructions in the code.
    
    **Guarding of pragmas**
    
    The used pragmas are not standardised but depend on the compilers.
    Therefore, we use pragmas specific to the currently detected compiler to
    avoid errors about unknown pragmas
    
    [1]: https://docs.python.org/3/c-api/structures.html
    [2]: https://stackoverflow.com/a/10264563
    
    UPD achaikou:
    backport-1.x branch: cherry pick required solving merge conflicts.
    Originally cherry picked from commit
    8c0102abdea591715d519d51cd7695f6f7c93191

--- python/segyio/segyio.cpp
+++ python/segyio/segyio.cpp
@@ -22,6 +22,13 @@
 #define IS_PY3K
 #endif
 
+#if defined(__GNUC__) && !defined(__clang__)
+#define IS_GCC
+#endif
+#if defined(__clang__)
+#define IS_CLANG
+#endif
+
 namespace {
 
 std::string segy_errstr( int err ) {
@@ -1336,6 +1343,16 @@ PyObject* rotation( segyiofd* self, PyObject* args ) {
     return PyFloat_FromDouble( rotation );
 }
 
+#ifdef IS_CLANG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcast-function-type"
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
+#endif
+#ifdef IS_GCC
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-function-type"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
 PyMethodDef methods [] = {
     { "segyopen", (PyCFunction) fd::segyopen, METH_NOARGS, "Open file." },
     { "segymake", (PyCFunction) fd::segycreate,
@@ -1376,9 +1393,24 @@ PyMethodDef methods [] = {
 
     { NULL }
 };
+#ifdef IS_GCC
+#pragma GCC diagnostic pop
+#endif
+#ifdef IS_CLANG
+#pragma clang diagnostic pop
+#endif
 
 }
 
+
+#ifdef IS_CLANG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
+#endif
+#ifdef IS_GCC
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
 PyTypeObject Segyiofd = {
     PyVarObject_HEAD_INIT( NULL, 0 )
     "_segyio.segyfd",               /* name */
@@ -1417,6 +1449,12 @@ PyTypeObject Segyiofd = {
     0,                              /* tp_dictoffset */
     (initproc)fd::init,             /* tp_init */
 };
+#ifdef IS_GCC
+#pragma GCC diagnostic pop
+#endif
+#ifdef IS_CLANG
+#pragma clang diagnostic pop
+#endif
 
 PyObject* binsize( PyObject* ) {
     return PyLong_FromLong( segy_binheader_size() );
@@ -1583,6 +1621,16 @@ PyObject* format( PyObject* , PyObject* args ) {
     return out;
 }
 
+#ifdef IS_CLANG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcast-function-type"
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
+#endif
+#ifdef IS_GCC
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-function-type"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
 PyMethodDef SegyMethods[] = {
     { "binsize",  (PyCFunction) binsize,  METH_NOARGS, "Size of the binary header." },
     { "thsize",   (PyCFunction) thsize,   METH_NOARGS, "Size of the trace header."  },
@@ -1599,11 +1647,25 @@ PyMethodDef SegyMethods[] = {
 
     { NULL }
 };
+#ifdef IS_GCC
+#pragma GCC diagnostic pop
+#endif
+#ifdef IS_CLANG
+#pragma clang diagnostic pop
+#endif
 
 }
 
 /* module initialization */
 #ifdef IS_PY3K
+#ifdef IS_CLANG
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
+#endif
+#ifdef IS_GCC
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#endif
 static struct PyModuleDef segyio_module = {
         PyModuleDef_HEAD_INIT,
         "_segyio",   /* name of module */
@@ -1611,6 +1673,12 @@ static struct PyModuleDef segyio_module = {
         -1,  /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
         SegyMethods
 };
+#ifdef IS_GCC
+#pragma GCC diagnostic pop
+#endif
+#ifdef IS_CLANG
+#pragma clang diagnostic pop
+#endif
 
 PyMODINIT_FUNC
 PyInit__segyio(void) {
