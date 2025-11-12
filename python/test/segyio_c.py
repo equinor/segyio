7768990ce915433643f31542ecb151d430fb1e67
Alena Chaikouskaya <alec@equinor.com>
2025-11-12 17:43:28 +0100

    fixup! Use mapping in field_forall/field_foreach

--- python/test/segyio_c.py
+++ python/test/segyio_c.py
@@ -532,9 +532,9 @@ def read_traceheaders_forall(f, mmap):
     field = segyio.TraceField.INLINE_3D
 
     with pytest.raises(ValueError):
-        f.field_forall(attrs, 0, start, stop, 0, field)
+        f.field_forall(attrs, SEG00000_INDEX, start, stop, 0, field)
 
-    buf_handle = f.field_forall(attrs, 0, start, stop, step, field)
+    buf_handle = f.field_forall(attrs, SEG00000_INDEX, start, stop, step, field)
     numpy.testing.assert_array_equal(attrs, [5, 4, 3, 2, 1])
     assert buf_handle is attrs
 
@@ -548,9 +548,9 @@ def read_traceheaders_foreach(f, mmap):
     field = segyio.TraceField.CROSSLINE_3D
 
     with pytest.raises(ValueError):
-        f.field_foreach(numpy.empty(1, dtype=numpy.intc), 0, indices, field)
+        f.field_foreach(numpy.empty(1, dtype=numpy.intc), SEG00000_INDEX, indices, field)
 
-    buf_handle = f.field_foreach(attrs, 0, indices, field)
+    buf_handle = f.field_foreach(attrs, SEG00000_INDEX, indices, field)
     numpy.testing.assert_array_equal(attrs, [22, 24, 21, 23, 20])
     assert buf_handle is attrs
 
